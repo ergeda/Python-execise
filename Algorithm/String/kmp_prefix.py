92d0b13448eb01cc9a1542d7f9792865de09305f
ergeda <xyaming@gmail.com>
2016-11-17 17:10:34 +0900

    kmp prefix helper

---
+++ Algorithm/String/kmp_prefix.py
@@ -0,0 +1,62 @@
+
+def kmp_prefix(input_str):
+    '''
+    prefix helper for Knuth-Morris-Pratt (KMP)
+    :param input_str:
+    :return:
+    '''
+    prefix = [-1]
+    k = -1
+    for i in range(1, len(input_str)):
+        while k >= 0 and input_str[i] != input_str[k+1]:
+            k = prefix[k]
+        if input_str[i] == input_str[k+1]:
+            k += 1
+        prefix.append(k)
+
+    return prefix
+
+def is_repeated_substring_pattern(input_str):
+    '''
+    check if input_str is with repeated substring pattern
+    :param input_str:
+    :return:
+    '''
+    prefix = kmp_prefix(input_str)
+    latest_zero = -1
+    for i in range(0, len(prefix)):
+        if prefix[i] == 0:
+            latest_zero = i
+        elif latest_zero != -1 and prefix[i] != prefix[i-1]+1:
+            latest_zero = -1
+    return latest_zero != -1 and (len(prefix)-latest_zero) % latest_zero == 0
+
+
+# call inside the module
+input_str = 'abcbababcbababcbaaabcbababcbababcbaaabcbababcbababcba'
+print(kmp_prefix(input_str))
+print(is_repeated_substring_pattern(input_str))
+
+input_str = 'abababababababababcccccccccccabababab'
+print(kmp_prefix(input_str))
+print(is_repeated_substring_pattern(input_str))
+
+input_str = 'a'
+print(kmp_prefix(input_str))
+print(is_repeated_substring_pattern(input_str))
+
+input_str = 'aa'
+print(kmp_prefix(input_str))
+print(is_repeated_substring_pattern(input_str))
+
+input_str = 'aac'
+print(kmp_prefix(input_str))
+print(is_repeated_substring_pattern(input_str))
+
+input_str = 'aaa'
+print(kmp_prefix(input_str))
+print(is_repeated_substring_pattern(input_str))
+
+input_str = 'aac'
+print(kmp_prefix(input_str))
+print(is_repeated_substring_pattern(input_str))
